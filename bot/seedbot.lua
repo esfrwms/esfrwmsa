61c675ac15c45e8340ec8019f5afb9ac2afe2808
esfrwms <mmahsa2006@gmail.com>
2016-06-11 09:53:18 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {203853586},--Sudo users
+    sudo_users = {219960877},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
